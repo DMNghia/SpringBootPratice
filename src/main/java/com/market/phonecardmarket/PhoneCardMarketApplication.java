464fef78969a6cf7badef9eab85940a249752258
DMNghia <minhnghia15112002@gmail.com>
2023-07-29 15:39:15 +0700

    Create Project

---
+++ src/main/java/com/market/phonecardmarket/PhoneCardMarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.phonecardmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneCardMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneCardMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/phonecardmarket/PhoneCardMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.phonecardmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneCardMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
